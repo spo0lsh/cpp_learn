c318a0a0073bbe6cb1b5f20bbe838d9655227574
Krzysztof Krych <public@scorp.pl>
2013-10-30 14:27:24 +0100

    first probably good version of ftpclient

---
+++ codeblocks/abs1/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+class CClientFTP;
+class CExecuteCommand;
+class IAccessToData;
+class CFileInputOutput;
+class CSocketInputOutput;
+*/
+class CClientFTP
+{
+public:
+    void getOptionsFromCommandLine();
+    void parseCommand();
+};
+
+class IAccessToData
+{
+public:
+    virtual void readStream()=0;
+    virtual void writeStream()=0;
+    virtual void openStream()=0;
+    virtual void closeStream()=0;
+};
+
+class CFileInputOutput : public IAccessToData
+{
+public:
+    void readStream()
+    {
+        std::cout << "CFileInputOutput::readStream" << endl;
+    }
+    void writeStream()
+    {
+        std::cout << "CFileInputOutput::writeStream" << endl;
+    }
+    void openStream()
+    {
+        std::cout << "CFileInputOutput::openStream" << endl;
+    }
+    void closeStream()
+    {
+        std::cout << "CFileInputOutput::closeStream" << endl;
+    }
+};
+
+class CSocketInputOutput : public IAccessToData
+{
+public:
+    void readStream()
+    {
+        std::cout << "CSocketInputOutput::readStream" << endl;
+    }
+    void writeStream()
+    {
+        std::cout << "CSocketInputOutput::writeStream" << endl;
+    }
+    void openStream()
+    {
+        std::cout << "CSocketInputOutput::openStream" << endl;
+    }
+    void closeStream()
+    {
+        std::cout << "CSocketInputOutput::closeStream" << endl;
+    }
+};
+
+
+class CExecuteCommand
+{
+public:
+    void getFileFromServer()
+    {
+        std::cout << "CExecuteCommand::getFileFromServer" << endl;
+        CFileInputOutput *wsk1 = new CFileInputOutput;
+        wsk1->openStream();
+        delete wsk1;
+        CSocketInputOutput *wsk2 = new CSocketInputOutput;
+        wsk2->openStream();
+        delete wsk2;
+    }
+    void putFileFromServer()
+    {
+        std::cout << "CExecuteCommand::putFileFromServer" << endl;
+    }
+    void showFilesOnServer()
+    {
+        std::cout << "CExecuteCommand::showFilesOnServer" << endl;
+    }
+    void deleteFileOnServer()
+    {
+        std::cout << "CExecuteCommand::deleteFileOnServer" << endl;
+    }
+};
+
+
+int main()
+{
+    CExecuteCommand *wsk = new CExecuteCommand;
+    wsk->getFileFromServer();
+    delete wsk;
+    return 0;
+}
