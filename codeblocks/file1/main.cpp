e88a7a0a8eb8b3e98718fec745e38f7199a7194e
Krzysztof Krych <public@scorp.pl>
2013-11-13 15:20:01 +0100

    PoC read/write with protected access.

---
+++ codeblocks/file1/main.cpp
@@ -0,0 +1,25 @@
+// crt_fsopen.c
+// http://msdn.microsoft.com/en-us/library/8f30b0db.aspx
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <share.h>
+
+int main( void )
+{
+   FILE *stream;
+
+   // Open output file for writing. Using _fsopen allows us to
+   // ensure that no one else writes to the file while we are
+   // writing to it.
+    //
+   if( (stream = _fsopen( "outfile.txt", "wt", _SH_DENYWR )) != NULL )
+   {
+      fprintf( stream, "No one else in the network can write "
+                       "to this file until we are done.\n" );
+      fclose( stream );
+   }
+   // Now others can write to the file while we read it.
+   system( "type outfile" );
+}
