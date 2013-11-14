fe8cf16bf6fcb0b87bc09079908539d595b73719
Krzysztof Krych <public@scorp.pl>
2013-11-14 13:38:01 +0100

    send string to thread

--- codeblocks/threads1/main.cpp
+++ codeblocks/threads1/main.cpp
@@ -11,7 +11,18 @@ static unsigned __stdcall threadMain(void *args)
 {
     while(1)
     {
-        cout << "threadMain(): " << *(int *) args << endl; // casting for int;
+        cout << "threadMain(): " << *(int *) args << " GetCurrentThread(): " << GetCurrentThread << endl; // casting for int;
+        SuspendThread(GetCurrentThread());
+    }
+    return 0;
+}
+
+static unsigned __stdcall threadMain2(void* msg) // print text
+{
+    while(1)
+    {
+        std::string str = *reinterpret_cast<std::string*>(msg);
+        cout << "threadMain2(): " << str << " GetCurrentThread(): " << GetCurrentThread << endl; // casting for int;
         SuspendThread(GetCurrentThread());
     }
     return 0;
@@ -22,31 +33,12 @@ int main()
     string Text = "test";
     int i;
     HANDLE h_thread;
-    h_thread = (HANDLE)_beginthreadex(NULL, 0, threadMain, &i, CREATE_SUSPENDED, NULL);
+    //h_thread = (HANDLE)_beginthreadex(NULL, 0, threadMain, &i, CREATE_SUSPENDED, NULL);
+    h_thread = (HANDLE)_beginthreadex(NULL, 0, threadMain2, &Text, CREATE_SUSPENDED, NULL);
     i = 10;
-    ResumeThread(h_thread);
-    WaitForSingleObject( h_thread, 5000 );
-    i = 20;
-    ResumeThread(h_thread);
-    WaitForSingleObject( h_thread, 5000 );
-    i = 30;
-    ResumeThread(h_thread);
-    WaitForSingleObject( h_thread, 5000 );
-    i = 40;
-    ResumeThread(h_thread);
-    DWORD res = WaitForSingleObject(h_thread, 0);
-    switch (res)
-    {
-        case WAIT_OBJECT_0:
-            printf("The thread has finished\n");
-            break;
-        case WAIT_TIMEOUT:
-            printf("The thread is still running\n");
-            break;
-        default:
-            printf("Unknown error (shouldn't happen)\n");
-            break;
+    DWORD res = ResumeThread(h_thread);
+    //cout << "res = " << res << endl;
+    DWORD res2 = WaitForSingleObject( h_thread, 5000 );
 
-    }
     return 0;
 }
