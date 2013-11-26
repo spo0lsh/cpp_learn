36cab28bebb2de19c09d7d9ab211076f6018ca5d
Krzysztof Krych <public@scorp.pl>
2013-11-26 11:55:15 +0100

    almost implemented Database

---
+++ codeblocks/FTPserver/DatabaseOperations.cpp
@@ -0,0 +1,33 @@
+#include "DatabaseOperations.h"
+
+using namespace std;
+
+void CDatabaseOperations::createDatabaseStructure()
+{
+
+    vector <vector<string> > DB;
+}
+
+//int CDatabaseOperations::addUserToDatabase(std::string, std::string )
+//{
+//    vector<string> user;
+//    user.push_back("user1");
+//    user.push_back("pass1");
+//    DB.push_back(user);
+//    user.clear();
+//    user.push_back("user2");
+//    user.push_back("pass2");
+//    DB.push_back(user);
+//    user.clear();
+//    user.push_back("user3");
+//    user.push_back("pass3");
+//    DB.push_back(user);
+//    user.clear();
+//}
+
+//void CDatabaseOperations::debugPrint()
+//{
+//    std::cout << "User " << " " << DB[0][0] << " password " << DB[0][1] << std::endl;
+//    std::cout << "User " << " " << DB[1][0] << " password " << DB[1][1] << std::endl;
+//    std::cout << "User " << " " << DB[2][0] << " password " << DB[2][1] << std::endl;
+//}
--- codeblocks/FTPserver/DatabaseOperations.h
+++ codeblocks/FTPserver/DatabaseOperations.h
@@ -1,6 +1,20 @@
 #ifndef DATABASEOPERATIONS_H_INCLUDED
 #define DATABASEOPERATIONS_H_INCLUDED
 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
 
+class CDatabaseOperations
+{
+public:
+    void createDatabaseStructure();
+    int compareLoginAndPassword(std::string, std::string );
+    int addUserToDatabase(std::string, std::string );
+    std::string readUsersFromFile(std::string );
+    void parseData(std::string );
+};
 
 #endif // DATABASEOPERATIONS_H_INCLUDED
